188bedb693731607697fac5a27dbbe7fd1e95156
Kaito-Dogi <kendobu0405@gmail.com>
2021-01-28 00:50:34 +0900

    Initial commit

---
+++ app/src/main/java/app/doggy/count/MainActivity.kt
@@ -0,0 +1,55 @@
+package app.doggy.count
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    //カウントする変数の定義。
+    var countNum = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //クリックリスナの設定。
+        plusButton.setOnClickListener(OnClickListener())
+        minusButton.setOnClickListener(OnClickListener())
+        clearButton.setOnClickListener(OnClickListener())
+    }
+
+    private inner class OnClickListener: View.OnClickListener {
+        override fun onClick(view: View) {
+
+            //ボタンのidによる場合分け。
+            when(view.id) {
+                R.id.plusButton -> {
+                    countNum += 1
+                    numberTextView.text = countNum.toString()
+                }
+
+                R.id.minusButton -> {
+                    countNum -= 1
+                    numberTextView.text = countNum.toString()
+                }
+
+                R.id.clearButton -> {
+                    countNum = 0
+                    numberTextView.text = countNum.toString()
+                }
+            }
+
+            //countNumの値が3の倍数の時、緑色にする。
+            if (countNum%3 == 0) {
+                numberTextView.setTextColor(ContextCompat.getColor(applicationContext, R.color.teal_700))
+            } else {
+                numberTextView.setTextColor(Color.parseColor("#333333"))
+            }
+
+        }
+    }
+}
